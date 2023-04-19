c6b2150e80345a50ba1bd5649477917e62fd187c
Aleksey <Scherbakovaleksey2005@mail.ru>
2023-04-19 21:16:16 +0700

    starts

---
+++ app/src/main/java/com/example/sherbakov_shvenk_pr_21102_pr5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sherbakov_shvenk_pr_21102_pr5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
